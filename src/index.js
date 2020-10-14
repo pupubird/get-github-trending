53f7d250ef36a36777be4ab8429dbd127b8002c5
pupubird <rainchai4240@gmail.com>
2020-10-14 17:09:10 +0800

    Add /languages /spoken_languages api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const scrape = require('./scrape');
 const validate = require('./validate');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0'
@@ -13,6 +14,16 @@ app.get('/repositories', async (req, res) => {
     const data = await scrape(language, since, spoken_language_code);
 
     res.send(data);
+})
+app.get('/languages', async (req, res) => {
+    const stream = fs.readFileSync(process.cwd() + '/src/languages.json');
+    const languages = JSON.parse(stream)
+    res.send(languages)
+})
+app.get('/spoken_languages', async (req, res) => {
+    const stream = fs.readFileSync(process.cwd() + '/src/spoken-languages-code.json');
+    const languages = JSON.parse(stream)
+    res.send(languages)
 })
 
 app.listen(PORT, HOST, console.log.bind(this, `Listening port ${PORT} host ${HOST}`))
