3732ae4566de4ba476afe2840642bcf307db568a
pupubird <rainchai4240@gmail.com>
2020-10-14 19:03:10 +0800

    Housekeeping

--- src/scrape.js
+++ src/scrape.js
@@ -1,6 +1,43 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
+module.exports.developers = async (language = "", since = "") {
+    const url = `https://github.com/trending/developers${language ? `/${language}` : ''}?since=${since}`
+    const data = await fetch(url);
+    const $ = cheerio.load(await data.text());
+    return $('.Box').find('.Box-row').get().map(repo => {
+        const $repo = $(repo);
+
+        const $username = $repo.find('.link-gray');
+        const username = $username.text().trim();
+
+        const $name = $username.parent().prev();
+        const name = $name.text().trim();
+
+        const $org_name = $repo.find('.octicon-organization');
+        const org_name = $org_name.text();
+
+        const $repo_name = $repo.find('.octicon-repo').parent();
+        const repo_name = $repo_name.text().trim();
+
+        const $description = $repo_name.parent().next();
+        const description = $description.text().trim();
+
+        return {
+            username,
+            name,
+            type: org_name ? 'organization' : 'user',
+            url: `https://github.com/${username}`,
+            avatar: `https://github.com/${username}.png?size=120`,
+            repo: {
+                repo_name,
+                description
+            }
+        }
+    });
+}
+
+
 module.exports = async (language = "", since = "", spoken_language_code = "") => {
     const url = `https://github.com/trending${language ? `/${language}` : ''}?since=${since}&spoken_language_code=${spoken_language_code}`
     const data = await fetch(url);
---
+++ src/scrape_developer.js
@@ -0,0 +1,3 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
