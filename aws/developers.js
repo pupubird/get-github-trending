7dc38230e3a2efbcc7fc5759b59f830400182299
pupubird <rainchai4240@gmail.com>
2020-10-14 19:05:46 +0800

    Add developes api

---
+++ aws/developers.js
@@ -0,0 +1,18 @@
+const { developers } = require('../src/scrape');
+const { validate } = require('../src/validate');
+
+exports.handler = async (event) => {
+    const { language, since } = event["queryStringParameters"] || { language: "", since: "" };
+
+    if (!validate(language, since)) return {
+        statusCode: 400,
+        body: "Bad request",
+    }
+
+    const data = await developers(language, since);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+};
