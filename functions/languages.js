30912a7eaed3ec28bbc0217acfd0e9acb69d60ab
pupubird <rainchai4240@gmail.com>
2020-10-18 15:39:39 +0800

    Housekeeping

--- functions/languages.js
+++ functions/languages.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 module.exports = async (event) => {
-    const data = fs.readFileSync(process.cwd() + '/src/languages.json')
+    const data = fs.readFileSync(process.cwd() + '/src/languages.json', "utf8")
     return {
         statusCode: 200,
         body: data,
--- functions/spoken_languages.js
+++ functions/spoken_languages.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 module.exports = async (event) => {
-    const data = fs.readFileSync(process.cwd() + '/src/spoken-languages-code.json')
+    const data = fs.readFileSync(process.cwd() + '/src/spoken-languages-code.json', "utf8")
     return {
         statusCode: 200,
         body: data,
