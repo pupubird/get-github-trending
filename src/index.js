35ecf5eadca19f287bf6249622294965437d3fa8
pupubird <rainchai4240@gmail.com>
2021-02-23 21:10:40 +0800

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const scrape = require('./scrape');
-const { developers } = require('./scrape');
-const validate = require('./validate');
-const fs = require('fs');
+const scrape = require("./scrape");
+const cors = require("cors");
+const { developers } = require("./scrape");
+const validate = require("./validate");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 5000;
-const HOST = '0.0.0.0'
+const HOST = "0.0.0.0";
 
+app.use(cors());
 app.use(validate);
 
-app.get('/repositories', async (req, res) => {
-    const { language, since, spoken_language_code } = req.query;
-    const data = await scrape(language, since, spoken_language_code);
+app.get("/repositories", async (req, res) => {
+  const { language, since, spoken_language_code } = req.query;
+  const data = await scrape(language, since, spoken_language_code);
 
-    res.send(data);
-})
+  res.send(data);
+});
 
-app.get('/developers', async (req, res) => {
-    const { language, since } = req.query;
-    const data = await developers(language, since);
-    res.send(data);
-})
+app.get("/developers", async (req, res) => {
+  const { language, since } = req.query;
+  const data = await developers(language, since);
+  res.send(data);
+});
 
-app.get('/languages', async (req, res) => {
-    const stream = fs.readFileSync(process.cwd() + '/src/languages.json');
-    const languages = JSON.parse(stream)
-    res.send(languages)
-})
+app.get("/languages", async (req, res) => {
+  const stream = fs.readFileSync(process.cwd() + "/src/languages.json");
+  const languages = JSON.parse(stream);
+  res.send(languages);
+});
 
-app.get('/spoken_languages', async (req, res) => {
-    const stream = fs.readFileSync(process.cwd() + '/src/spoken-languages-code.json');
-    const languages = JSON.parse(stream)
-    res.send(languages)
-})
+app.get("/spoken_languages", async (req, res) => {
+  const stream = fs.readFileSync(
+    process.cwd() + "/src/spoken-languages-code.json"
+  );
+  const languages = JSON.parse(stream);
+  res.send(languages);
+});
 
-app.listen(PORT, HOST, console.log.bind(this, `Listening port ${PORT} host ${HOST}`))
+app.listen(
+  PORT,
+  HOST,
+  console.log.bind(this, `Listening port ${PORT} host ${HOST}`)
+);
